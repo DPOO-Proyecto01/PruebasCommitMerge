eca6cb04a5e6708872a0439fea1874c14498c020
Emmanuel Blanco <ef.blanco@uniandes.edu.co>
2024-04-13 15:44:53 -0500

    Primer commit

---
+++ Pruebas/src/paquete1/Clase1.java
@@ -0,0 +1,16 @@
+package paquete1;
+
+public class Clase1 
+{
+	public String atributo_1 = "Algo";
+	public int sumarHastaN (int n)
+	{
+		int suma = 0;
+		for (int i = 0; n<i;i++)
+		{
+			suma+=i;
+		}
+		return suma;
+	}
+
+}
