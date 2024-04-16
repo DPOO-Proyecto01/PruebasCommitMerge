9c6f3d9cdafc80091a6478e3d1e400e7994d23f8
a.tellou <a.tellou@ISC248.sis.virtual.uniandes.edu.co>
2024-04-16 12:42:30 -0500

    Prueba commit a.tellou

---
+++ Pruebas/src/paquete1/Clase3.java
@@ -0,0 +1,14 @@
+package paquete1;
+
+public class Clase3 {
+	private String test;
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+
+}
